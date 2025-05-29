740835c23904e4bcddcf7daceaec84120e841cf6
JabalThoriq16 <jabal.thoriq16@mhs.uinjkt.ac.id>
2025-05-29 18:06:56 +0700

    versi final 2

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url"; // untuk __dirname di ES Module
+
 import "./src/config/mysql.js"; // koneksi Sequelize
 import router from "./src/routes/route.js"; // import router utama
 
@@ -11,8 +14,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Konversi __dirname di ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Swagger setup
 const swaggerPath = path.resolve(__dirname, "post-man", "swagger.yaml");
+const swaggerDocument = YAML.load(swaggerPath);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
@@ -21,7 +29,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router); // semua route dimulai dengan /api
 
-app.all(/.*/, (req, res) => {
+// Middleware 404
+app.all("*", (req, res) => {
   res.status(404).json({ code: 404, message: "Not Found" });
 });
 
